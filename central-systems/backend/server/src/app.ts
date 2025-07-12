20201fad4ec7823993c945e3f6902dafff0e2f5f
corneliusdevs <owolabitosin020@gmail.com>
2025-07-11 23:27:48 -0700

    :recylce: update cors configuration

--- central-systems/backend/server/src/app.ts
+++ central-systems/backend/server/src/app.ts
@@ -18,7 +18,7 @@ app.use(cookiesParser()); // allows us to read and set cookies
 
 // Define CORS options (optional)
 const corsOptions = {
-  origin: 'http://localhost:3000', // specify the allowed domain or use '*' to allow all
+  origin: 'https://central-systems.wordsanctuaryglobal.com', // specify the allowed domain or use '*' to allow all
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // allowed headers
   credentials: true, // allow cookies to be sent across domains
